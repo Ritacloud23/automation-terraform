b6c98c091e55f0722650930dc956121ba34e4bde
Rita <ugwuanyinnenna43@gmail.com>
2025-11-28 20:57:33 +0000

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "rita-fintch-bucket"
-#     key    = "tt-aws/production/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "rita-fintch-bucket"
+    key    = "tt-aws/production/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
