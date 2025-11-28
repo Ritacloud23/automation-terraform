2646d0bf0087357daeeab4e1b675cffd2f133887
Rita <ugwuanyinnenna43@gmail.com>
2025-11-28 16:46:40 +0000

    added detailes

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "rita-fintch-bucket"
-#     key            = "tt-aws/production/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-lock"     # ADD THIS
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "rita-fintch-bucket"
+    key            = "tt-aws/production/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"     # ADD THIS
+    encrypt        = true
+  }
+}
